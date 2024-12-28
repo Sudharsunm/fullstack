129ec3f102b88783bbf010e26e0b38e956e013f6
Sudharsunm <sudharsunmahalingam@gmail.com>
2024-12-28 11:45:08 +0530

    javascript

---
+++ javascript/conditional_statement.js
@@ -0,0 +1,25 @@
+var a=10
+if(a%2!=0){
+    console.log(a," is odd")
+}
+else{
+    if(a==0){
+        console.log(a,"is zero")
+    }
+    console.log(a," is even")
+}
+//2
+a++
+console.log(a)
+//3
+--a
+b=2
+add=a+b
+sub=a-b
+m=a-b
+d=a/b
+console.log(add)
+console.log(sub)
+console.log(m)
+console.log(d)
+//4.relational operator
